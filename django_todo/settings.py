1a218283c30a32143821f6acd65ac0ee1be7b875
ReubensIdeas <>
2023-09-08 11:20:54 +0000

    secret key updated

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,12 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z!j748)wy3hsypnzdnf1*0+qm8o!ba$cfk&$*22o!f20f8=+5u'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'django-insecure-z!j748)wy3hsypnzdnf1*0+qm8o!ba$cfk&$*22o!f20f8=+5u')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['reubensideas-django-todo-4a84943cd52e.herokuapp.com']
+ALLOWED_HOSTS = [os_environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
